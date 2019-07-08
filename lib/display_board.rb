92b228cdcac4b6090204028267082068e7acdf0e
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-08 20:41:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,41 +4,41 @@
 board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
 
 
-# def display_board(board)
+def display_board
 
-#   print " " + board[0] + " " + "|" + " " + board[1] + " " + "|" + " " + board[2] + " "
+  print "+ " " + "|" + " " + board[1] + " " + "|" + " " + board[2] + " "
   
-#   puts ""
+  puts ""
   
-#   print "-----------"
+  print "-----------"
   
-#   puts ""
+  puts ""
   
-#   print " " + board[3] + " " + "|" + " " + board[4] + " " + "|" + " " + board[5] + " "
+  print " " + board[3] + " " + "|" + " " + board[4] + " " + "|" + " " + board[5] + " "
   
-#   puts ""
+  puts ""
   
-#   print "-----------"
+  print "-----------"
   
-#   puts ""
+  puts ""
   
-#   print " " + board[6] + " " + "|" + " " + board[7] + " " + "|" + " " + board[8] + " "
-  
-# end
-
-
-def display_board(board)
-  
-  puts " " + board[0] + " | " + board[1] + " | " + board[2] + " "
-  
-  puts "----------"
-  
-  puts " " + board[3] + " | " + board[4] + " | " + board[5] + " "
-  
-  puts "----------"
-  
-  puts " " + board[6] + " | " + board[7] + " | " + board[8] + " "
+  print " " + board[6] + " " + "|" + " " + board[7] + " " + "|" + " " + board[8] + " "
   
 end
 
-display_board(board)
+
+# def display_board(board)
+  
+#   puts " " + board[0] + " | " + board[1] + " | " + board[2] + " "
+  
+#   puts "----------"
+  
+#   puts " " + board[3] + " | " + board[4] + " | " + board[5] + " "
+  
+#   puts "----------"
+  
+#   puts " " + board[6] + " | " + board[7] + " | " + board[8] + " "
+  
+# end
+
+display_board
