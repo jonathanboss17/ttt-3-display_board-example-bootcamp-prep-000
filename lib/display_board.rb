ac209b7388f345b156bf48c85257815310057ea8
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-08 20:29:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,19 +1,25 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-print "   " + "|" + "   " + "|" + "   "
+def displayboard
 
-puts ""
+  print "   " + "|" + "   " + "|" + "   "
+  
+  puts ""
+  
+  print "-----------"
+  
+  puts ""
+  
+  print "   " + "|" + "   " + "|" + "   "
+  
+  puts ""
+  
+  print "-----------"
+  
+  puts ""
+  
+  print "   " + "|" + "   " + "|" + "   "
+  
+end
 
-print "-----------"
-
-puts ""
-
-print "   " + "|" + "   " + "|" + "   "
-
-puts ""
-
-print "-----------"
-
-puts ""
-
-print "   " + "|" + "   " + "|" + "   "
+displayboard
